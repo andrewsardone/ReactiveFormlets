ad21296a2750c4dffc9a755aa09658cf4a85e173
Jonathan Sterling <jonsterling@me.com>
2012-12-18 12:18:34 -0800

    Delete RFMenuRow, which is totally broken
    Will re-add once I've understood why.

--- iOS Formlets/JSViewController.m
+++ iOS Formlets/JSViewController.m
@@ -11,27 +11,23 @@
 #import "RFTableForm.h"
 #import "RFInputRow.h"
 #import "RFTableSection.h"
-#import "RFMenuRow.h"
 
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
 @protocol Animal <RFFormlet>
-@property (readonly) id <Text> name;
-@property (readonly) id <Text> nickName;
-@property (readonly) id <Text> favoriteFood;
-@property (readonly) id <Animal> friend;
-@property (readonly) id <Number> age;
-+ (instancetype)name:(id <Text>)name nickName:(id <Text>)nickName favoriteFood:(id <Text>)food age:(id <Number>)age;
-+ (instancetype)name:(id <Text>)name nickName:(id <Text>)nickName friend:(id <Animal>)friend;
+- (id<Text>)name;
+- (id<Text>)nickName;
+- (id<Text>)favoriteFood;
+- (id<Animal>)friend;
+- (id<Number>)age;
++ (instancetype)name:(id<Text>)name nickName:(id<Text>)nickName favoriteFood:(id<Text>)food age:(id<Number>)age;
++ (instancetype)name:(id<Text>)name nickName:(id<Text>)nickName friend:(id<Animal>)friend;
 @end
 
 @protocol Pets <RFFormlet>
-@property (strong, readonly) id <Animal> dog;
-@property (strong, readonly) id <Animal> cat;
+- (id<Animal>)dog;
+- (id<Animal>)cat;
 + (instancetype)dog:(id<Animal>)dog cat:(id<Animal>)cat;
-@end
-
-@interface JSViewController () <JSMenuRowDelegate>
 @end
 
 @implementation JSViewController {
@@ -41,49 +37,27 @@
 - (void)loadView
 {
     Class AnimalModel = [RFReifiedProtocol model:@protocol(Animal)];
-    id <Animal> rover = [AnimalModel name:@"Rover"
-                                 nickName:@"Peanut"
+    id<Animal> tucker = [AnimalModel name:@"Tucker"
+                                 nickName:@"Tuckey"
                              favoriteFood:@"kibbles"
                                       age:@6];
-    id <Animal> tucker = [AnimalModel name:@"Tucker"
-                                  nickName:@"Tuckey"
-                                    friend:rover];
-
-    RFInputRow <Text> *textRow = [RFInputRow text];
-
-    Class AnimalMenu = [RFMenuRow model:@protocol(Animal)];
-    RFMenuRow <Animal> *animalMenu = [AnimalMenu name:[textRow placeholder:@"name"]
-                                             nickName:[textRow placeholder:@"nickname"]
-                                         favoriteFood:[textRow placeholder:@"favoriteFood"]
-                                                  age:[[RFInputRow number] placeholder:@"age"]];
-    animalMenu.delegate = self;
+    
+    RFInputRow<Text> *textRow = [RFInputRow text];
+    RFInputRow<Number> *numberRow = [RFInputRow number];
 
     Class AnimalSection = [RFTableSection model:@protocol(Animal)];
     RFTableSection <Animal> *animalSection = [AnimalSection name:[textRow placeholder:@"name"]
                                                         nickName:[textRow placeholder:@"nickname"]
-                                                          friend:[animalMenu withTitle:@"friend"]];
+                                                    favoriteFood:[textRow placeholder:@"food"]
+                                                             age:[numberRow placeholder:@"age"]];
 
     Class PetsTable = [RFTableForm model:@protocol(Pets)];
     _form = [PetsTable dog:[[animalSection title:@"Doggy"] withValue:tucker]
                        cat:[animalSection title:@"Kitty"]];
 
-    RAC(self.title) = [_form.dog.friend map:^(id <Animal> buddy) {
-        return [NSString stringWithFormat:@"%@ is %@", buddy.name, buddy.age];
-    }];
-
-    [_form.dog subscribeNext:^(id x) {
-        NSLog(@"%@", x);
-    }];
+    RAC(self.title) = _form.dog.name;
 
     self.view = _form.view;
-}
-
-
-#pragma mark - JSMenuRowDelegate
-
-- (void)menuRow:(RFMenuRow *)row displayViewController:(UITableViewController *)controller
-{
-    [self.navigationController pushViewController:controller animated:YES];
 }
 
 @end
