38976a602134888433b1b7183f57a3f3f4965592
Jonathan Sterling <jonsterling@me.com>
2012-12-18 16:42:55 -0800

    Correct storage types for NSInvocation arguments

--- iOS Formlets/RFReifiedProtocol.m
+++ iOS Formlets/RFReifiedProtocol.m
@@ -8,24 +8,25 @@
 
 #import "RFReifiedProtocol.h"
 #import "RFOrderedDictionary.h"
-#import <ReactiveCocoa/RACBehaviorSubject.h>
+#import <ReactiveCocoa/RACObjCRuntime.h>
 #import <objc/runtime.h>
 
 @implementation RFReifiedProtocol
 
 + (Class)model:(Protocol *)model {
 	NSString *className = [NSString stringWithFormat:@"%@_%s", self, protocol_getName(model)];
+
 	Class modelClass = objc_getClass(className.UTF8String);
 	if (modelClass != nil) return modelClass;
-
 	modelClass = objc_allocateClassPair([self class], className.UTF8String, 0);
+
 	objc_registerClassPair(modelClass);
 	class_addProtocol(modelClass, model);
 
 	Class metaclass = object_getClass(modelClass);
-
-	Protocol *(^model_block)() = ^{ return model; };
-	IMP model_imp = imp_implementationWithBlock(model_block);
+	IMP model_imp = imp_implementationWithBlock(^{
+		return model;
+	});
 
 	char const *typeEncoding = method_getTypeEncoding(class_getClassMethod(modelClass, @selector(model)));
 	class_replaceMethod(metaclass, @selector(model), model_imp, typeEncoding);
@@ -56,7 +57,7 @@
 
 	RFOrderedDictionary *dictionary = [[RFOrderedDictionary new] modify:^(id<RFMutableOrderedDictionary> dict) {
 		for (int i = 2; i < invocation.methodSignature.numberOfArguments; ++i) {
-			__autoreleasing id outArgument = nil;
+			__unsafe_unretained id outArgument = nil;
 			[invocation getArgument:&outArgument atIndex:i];
 
 			NSString *key = [keys objectAtIndex:i-2];
@@ -64,8 +65,9 @@
 		}
 	}];
 
-	__autoreleasing RFReifiedProtocol *modelObject = [[self alloc] initWithOrderedDictionary:dictionary];
-	invocation.returnValue = &modelObject;
+	invocation.returnValue = &(__unsafe_unretained id){
+		[[self alloc] initWithOrderedDictionary:dictionary]
+	};
 }
 
 
@@ -77,8 +79,9 @@
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
 	NSString *key = NSStringFromSelector(anInvocation.selector);
-	id value = [self valueForKey:key];
-	anInvocation.returnValue = (__strong id *)&value;
+	anInvocation.returnValue = &(__unsafe_unretained id){
+		[self valueForKey:key]
+	};
 }
 
 - (BOOL)respondsToSelector:(SEL)aSelector {
